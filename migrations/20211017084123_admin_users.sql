a56cbe4e20dacf89bf19764697ed0075ee5cccbb
realaravinth <realaravinth@batsense.net>
2021-10-17 21:40:18 +0530

    rename admin_users.name to username

--- migrations/20211017084123_admin_users.sql
+++ migrations/20211017084123_admin_users.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS admin_users (
-	name VARCHAR(100) NOT NULL UNIQUE,
----	TODO change to username VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL UNIQUE,
 	email VARCHAR(100) UNIQUE DEFAULT NULL,
 	email_verified BOOLEAN DEFAULT NULL,
     secret varchar(50) NOT NULL UNIQUE,
