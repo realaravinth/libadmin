1b1bd2e980888615141156e984abc4c5aa3e6a22
realaravinth <realaravinth@batsense.net>
2021-10-19 21:31:56 +0530

    group all traits udner one master trait

--- database/db-sqlx-sqlite/src/errors.rs
+++ database/db-sqlx-sqlite/src/errors.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use db_core::dev::*;
 use sqlx::Error;
 
-pub fn map_register_err(e: Error) -> DBError<Error> {
+pub fn map_register_err(e: Error) -> DBError {
     if let Error::Database(err) = e {
         if err.code() == Some(Cow::from("2067")) {
             let msg = err.message();
@@ -14,12 +14,12 @@ pub fn map_register_err(e: Error) -> DBError<Error> {
             } else if msg.contains("admin_users.secret") {
                 DBError::DuplicateSecret
             } else {
-                DBError::DBError(Error::Database(err).into())
+                DBError::DBError(format!("{:?}", Error::Database(err)))
             }
         } else {
-            DBError::DBError(Error::Database(err).into())
+            DBError::DBError(format!("{:?}", Error::Database(err)))
         }
     } else {
-        DBError::DBError(e)
+        DBError::DBError(format!("{:?}", e))
     }
 }
--- database/db-sqlx-sqlite/src/lib.rs
+++ database/db-sqlx-sqlite/src/lib.rs
@@ -3,8 +3,6 @@ use db_core::dev::*;
 use sqlx::sqlite::SqlitePool;
 use sqlx::sqlite::SqlitePoolOptions;
 
-pub mod account;
-pub mod auth;
 pub mod errors;
 #[cfg(test)]
 pub mod tests;
@@ -17,8 +15,6 @@ pub struct ConnectionOptions {
     pub pool_options: SqlitePoolOptions,
     pub url: String,
 }
-
-impl LibAdminDatabase for Database {}
 
 pub mod dev {
     pub use super::errors::*;
@@ -33,38 +29,192 @@ pub mod prelude {
     pub use db_core::prelude::*;
 }
 
-impl DBOps for Database {}
 #[async_trait]
 impl Connect for ConnectionOptions {
-    type Error = sqlx::Error;
     type Pool = Database;
-    async fn connect(self) -> DBResult<Self::Pool, Self::Error> {
+    async fn connect(self) -> DBResult<Self::Pool> {
         let pool = self
             .pool_options
             .connect(&self.url)
             .await
-            .map_err(DBError::DBError)?;
+            .map_err(|e| DBError::DBError(format!("{:?}", e)))?;
         Ok(Database { pool })
     }
 }
 
+use dev::*;
+
 #[async_trait]
-impl GetConnection for Database {
-    type Conn = SqlitePool;
-    type Error = sqlx::Error;
-    async fn get_conn(&self) -> DBResult<Self::Conn, Self::Error> {
-        Ok(self.pool.clone())
+impl Migrate for Database {
+    async fn migrate(&self) -> DBResult<()> {
+        sqlx::migrate!("./migrations/")
+            .run(&self.pool)
+            .await
+            .map_err(|e| DBError::DBError(format!("{:?}", e)))?;
+        Ok(())
     }
 }
 
 #[async_trait]
-impl Migrate for Database {
-    type Error = sqlx::migrate::MigrateError;
-    async fn migrate(&self) -> DBResult<(), <Self as Migrate>::Error> {
-        sqlx::migrate!("./migrations/")
-            .run(&self.pool)
-            .await
-            .map_err(DBError::DBError)?;
+impl LibAdminDatabase for Database {
+    async fn email_login(&self, email: &str) -> DBResult<Creds> {
+        sqlx::query_as!(
+            Creds,
+            r#"SELECT username, password  FROM admin_users WHERE email = ($1)"#,
+            email,
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| match e {
+            Error::RowNotFound => DBError::AccountNotFound,
+            e => DBError::DBError(format!("{:?}", e)),
+        })
+    }
+
+    async fn username_login(&self, username: &str) -> DBResult<Password> {
+        sqlx::query_as!(
+            Password,
+            r#"SELECT password  FROM admin_users WHERE username = ($1)"#,
+            username,
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| match e {
+            Error::RowNotFound => DBError::AccountNotFound,
+            e => DBError::DBError(format!("{:?}", e)),
+        })
+    }
+
+    async fn email_register(&self, payload: &EmailRegisterPayload) -> DBResult<()> {
+        sqlx::query!(
+            "insert into admin_users 
+        (username , password, email, secret) values ($1, $2, $3, $4)",
+            payload.username,
+            payload.password,
+            payload.email,
+            payload.secret,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(map_register_err)?;
         Ok(())
+    }
+
+    async fn username_register(&self, payload: &UsernameRegisterPayload) -> DBResult<()> {
+        sqlx::query!(
+            "INSERT INTO admin_users 
+        (username , password,  secret) VALUES ($1, $2, $3)",
+            payload.username,
+            payload.password,
+            payload.secret,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(map_register_err)?;
+        Ok(())
+    }
+
+    async fn update_email(&self, payload: &UpdateEmailPayload) -> DBResult<()> {
+        sqlx::query!(
+            "UPDATE admin_users set email = $1
+        WHERE username = $2",
+            payload.email,
+            payload.username,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(map_register_err)?;
+        Ok(())
+    }
+    async fn update_password(&self, payload: &Creds) -> DBResult<()> {
+        sqlx::query!(
+            "UPDATE admin_users set password = $1
+        WHERE username = $2",
+            payload.password,
+            payload.username,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|e| DBError::DBError(format!("{:?}", e)))?;
+        Ok(())
+    }
+
+    async fn email_exists(&self, email: &str) -> DBResult<bool> {
+        let exists;
+        match sqlx::query!("SELECT id from admin_users WHERE email = $1", email)
+            .fetch_one(&self.pool)
+            .await
+        {
+            Ok(_) => exists = true,
+            Err(Error::RowNotFound) => exists = false,
+            Err(e) => return Err(DBError::DBError(format!("{:?}", e))),
+        };
+
+        Ok(exists)
+    }
+
+    async fn delete_account(&self, username: &str) -> DBResult<()> {
+        sqlx::query!("DELETE FROM admin_users WHERE username = ($1)", username,)
+            .execute(&self.pool)
+            .await
+            .map_err(|e| DBError::DBError(format!("{:?}", e)))?;
+        Ok(())
+    }
+
+    async fn username_exists(&self, username: &str) -> DBResult<bool> {
+        let exists;
+        match sqlx::query!("SELECT id from admin_users WHERE username = $1", username)
+            .fetch_one(&self.pool)
+            .await
+        {
+            Ok(_) => exists = true,
+            Err(Error::RowNotFound) => exists = false,
+            Err(e) => return Err(DBError::DBError(format!("{:?}", e))),
+        };
+
+        Ok(exists)
+    }
+
+    async fn update_username(&self, payload: &UpdateUsernamePayload) -> DBResult<()> {
+        sqlx::query!(
+            "UPDATE admin_users set username = $1 WHERE username = $2",
+            payload.new_username,
+            payload.old_username,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(map_register_err)?;
+        Ok(())
+    }
+    async fn update_secret(&self, username: &str, secret: &str) -> DBResult<()> {
+        sqlx::query!(
+            "UPDATE admin_users set secret = $1
+        WHERE username = $2",
+            secret,
+            username,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(map_register_err)?;
+        Ok(())
+    }
+
+    async fn get_secret(&self, username: &str) -> DBResult<String> {
+        struct Secret {
+            secret: String,
+        }
+        let secret = sqlx::query_as!(
+            Secret,
+            r#"SELECT secret  FROM admin_users WHERE username = ($1)"#,
+            username,
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| match e {
+            Error::RowNotFound => DBError::AccountNotFound,
+            e => DBError::DBError(format!("{:?}", e)),
+        })?;
+
+        Ok(secret.secret)
     }
 }
